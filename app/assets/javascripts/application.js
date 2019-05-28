e868fda1046a6d6e48546c1954a73b667c4f2f3b
Jad Azzam <jadazzam@me.com>
2019-05-28 16:40:09 +0200

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFkYXp6YW0iLCJhIjoiY2p3N3V0emJ5MG9mNTRibzRvZWtwbjEycyJ9.tNZ2WFYkQpfxWiZM044i7Q';
+const map = new mapboxgl.Map({
+  container: 'mapbox',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
